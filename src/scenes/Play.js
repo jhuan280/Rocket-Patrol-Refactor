e6b79f6c5b65064e8edfb0c69dc638672a97b986
Jackie Huang <jhuan280@ucsc.edu>
2023-10-12 15:21:47 -0700

    added movement to the spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,6 +12,7 @@ class Play extends Phaser.Scene {
     }
 
     create(){
+        //starfield
         this.starfied = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
         //this.add.text(20, 20, "Rocket Patrol Play");
@@ -26,15 +27,54 @@ class Play extends Phaser.Scene {
         //add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0,5, 0);
 
+        //add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize * 6, borderUISize * 4, 'spaceship', 0, 30).setOrigin(0,0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize * 3, borderUISize * 5 + borderPadding * 2, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize * 6 + borderPadding * 4, 'spaceship', 0, 10).setOrigin(0, 0);
+       
+        //keyboard inputs
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
+        
     }
 
     update(){
         this.starfied.tilePositionX -= 4;
         this.p1Rocket.update();
+
+        //updates spaceships (x3)
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
+
+        //check collisions
+        if (this.checkCollision(this.p1Rocket, this.ship03)){
+            console.log('kaboom ship 03');
+        }
+
+        if (this.checkCollision(this.p1Rocket, this.ship02)){
+            console.log('kaboom ship 02');
+        }
+
+        if (this.checkCollision(this.p1Rocket, this.ship01)){
+            console.log('kaboom ship 01');
+        }
+    }
+
+    checkCollision(rocket, ship){
+        //simple AABB checking
+        if(rocket.x < ship.x + ship.width &&
+            rocket.x + rocket.width > ship.x &&
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship.y){
+                return true;
+            }
+
+        else{
+            return false;
+        }
     }
 }
